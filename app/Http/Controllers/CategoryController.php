3d633c0d2e8f1418e75755211a91e9309e72d623
Plus <serge@openmailbox.org>
2016-03-14 22:24:31 -0600

    routes and controller(crud) for categories

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Category::orderBy('id','ASC')->paginate(2);
+
+        return view('Category.index', ['categories' => $categories]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('Category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Category::create($request->all());
+        $request->session()->flash('message', 'Cateogira agregada exitosamente!');
+        return redirect()->route('category.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = 1;
+        $category = Category::findOrFail($id);
+        return view('Category.edit', ['category' => $category, 'user' => $user ] );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $cateogry = Category::findOrFail($id);
+        $cateogry->fill($request->all());
+        $cateogry->save();
+
+        $request->session()->flash('message','Cateogria actualizada!');
+
+        return redirect()->route('category.edit',[$id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return "Categoria Eliminada (No es cierto!)";
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function () {
+
+    Route::get('/categories', [
+        'uses' => 'CategoryController@index',
+        'as' => 'category.index'
+    ]);
+
+    Route::get('/category', [
+        'uses' => 'CategoryController@create',
+        'as' => 'category.create'
+    ]);
+    Route::post('/category', [
+        'uses' => 'CategoryController@store',
+        'as' => 'category.store'
+    ]);
+
+    Route::get('/ecategory/{id}/edit', [
+        'uses' => 'CategoryController@edit',
+        'as' => 'category.edit'
+    ])->where('id', '[0-9]+');
+
+    Route::post('/category/{id}/edit', [
+        'uses' => 'CategoryController@update',
+        'as' => 'category.update'
+    ])->where('id', '[0-9]+');
+
+    Route::get('/category/{id}/delete', [
+        'uses' => 'CategoryController@destroy',
+        'as' => 'category.destroy'
+    ])->where('id', '[0-9]+');
+
+
+});
