cbdec47afc6cde0db5f8069231dab72b5ed4dad9
Plus <serge@openmailbox.org>
2016-03-27 22:13:38 -0600

    EventController with CRUD methods

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Http\Requests\EventRequest;
+
+use App\Event;
+use App\Category;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+     public function create()
+     {
+         $categories = Category::lists('name', 'id');
+         return view('Event.create',['categories' => $categories]);
+     }
+
+     /**
+      * Store a newly created resource in storage.
+      *
+      * @param  \Illuminate\Http\Request  $request
+      * @return \Illuminate\Http\Response
+      */
+     public function store(EventRequest $request)
+     {
+         //dd($request->all());
+
+         Event::create($request->all());
+         $request->session()->flash('message', 'Evento registrado exitosamente!');
+         return redirect()->route('event.create');
+     }
+
+     /**
+      * Display the specified resource.
+      *
+      * @param  int  $id
+      * @return \Illuminate\Http\Response
+      */
+     public function show($id)
+     {
+         $user = 1;
+         $event = Event::findOrFail($id);
+         if (empty($event)) {
+             $event = null;
+         }
+         return view('event.show', ['event' => $event, 'user' => $user ] );
+     }
+
+     /**
+      * Show the form for editing the specified resource.
+      *
+      * @param  int  $id
+      * @return \Illuminate\Http\Response
+      */
+     public function edit($id)
+     {
+         $user = 1;
+         $event = Event::findOrFail($id);
+         $categories = Category::lists('name', 'id');
+         return view('event.edit', ['event' => $event, 'categories' => $categories, 'user' => $user ] );
+     }
+
+     /**
+      * Update the specified resource in storage.
+      *
+      * @param  \Illuminate\Http\Request  $request
+      * @param  int  $id
+      * @return \Illuminate\Http\Response
+      */
+     public function update(EventRequest $request, $id)
+     {
+         $event = Event::findOrFail($id);
+         $event->fill($request->all());
+         $event->save();
+
+         $request->session()->flash('message','Evento actualizado');
+
+         return redirect()->route('event.edit',[$id]);
+     }
+
+     /**
+      * Remove the specified resource from storage.
+      *
+      * @param  int  $id
+      * @return \Illuminate\Http\Response
+      */
+     public function destroy($id)
+     {
+         $event = Event::findOrFail($id);
+         if (! $event->delete()) {
+             return "Error al eliminar el evento";
+         }
+
+         return "Evento eliminado";
+     }
+}
