a35c7ddbe68e0a8e193ae7f59230e86d58399ff8
Plus <serge@openmailbox.org>
2016-05-05 16:44:29 -0500

    added debugbar

--- config/app.php
+++ config/app.php
@@ -149,6 +149,7 @@ return [
         * External Service Providers...
         */
         Collective\Html\HtmlServiceProvider::class,
+        'Barryvdh\Debugbar\ServiceProvider',
 
     ],
 
@@ -201,6 +202,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Debugbar' => 'Barryvdh\Debugbar\Facade',
 
     ],
 
